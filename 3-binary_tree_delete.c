631a3cef441eccde30ce712f2de0a582a8745818
force-push-main <10535@holbertonstudents.com>
2025-04-07 02:28:13 +0000

    added code for task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - entry point
+ *
+ * Description: deletes binary tree
+ * @tree: root of tree
+ * Return: returns void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	if (tree->left)
+	{
+		binary_tree_delete(tree->left);
+	}
+
+	if (tree->right)
+	{
+		binary_tree_delete(tree->right);
+	}
+
+	free(tree);
+}
