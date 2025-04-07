2edb68c93c714ba87f68983e6512f43f8ff9e8cf
force-push-main <10535@holbertonstudents.com>
2025-04-07 01:18:36 +0000

    added code for task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - entry point
+ *
+ * Description: creates new child node as left node of passed parent
+ * @parent: Pointer to the parent node
+ * @value: value to add to new node
+ * Return: returns pointer to new child
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *child;
+
+	child = malloc(sizeof(binary_tree_t));
+
+	if (!child)
+		return (NULL);
+
+	child->n = value;
+	child->right = NULL;
+
+	if (!parent)
+	{
+		child->parent = NULL;
+		child->left = NULL;
+		parent = child;
+		return (parent);
+	}
+
+	child->parent = parent;
+
+	if (parent->left)
+	{
+		parent->left->parent = child;
+		child->left = parent->left;
+		parent->left = child;
+		return (child);
+	}
+
+	parent->left = child;
+	child->left = NULL;
+
+	return (child);
+}
