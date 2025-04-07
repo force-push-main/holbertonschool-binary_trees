bc6a11b5c2a7c54eb0b9dd47852741ac27b2acc0
force-push-main <10535@holbertonstudents.com>
2025-04-07 07:12:19 +0000

    added code for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int h_left, h_right;
+	int left, right;
 
 	if (!tree)
 		return (0);
 
-	h_left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	h_right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 
-	return (h_left - h_right);
+	return (left - right);
 }
