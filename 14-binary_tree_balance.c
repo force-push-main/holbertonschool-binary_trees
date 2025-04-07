41a459a916423ca37cb050c0fb60092623b0a062
force-push-main <10535@holbertonstudents.com>
2025-04-07 07:48:25 +0000

    saving to pick up tomorrow

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
 
+int find_height(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+
+	left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+
+	return (left > right ? left : right);
+}
+
 /**
  * binary_tree_balance - entry point
  * @tree: root of tree
@@ -14,8 +27,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	left = tree->left ? 1 + find_height(tree->left) : 0;
+	right = tree->right ? 1 + find_height(tree->right) : 0;
 
 	return (left - right);
 }
