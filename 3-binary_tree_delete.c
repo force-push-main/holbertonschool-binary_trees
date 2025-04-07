ef58593e3e805dfd2f8c651e9e5cfdff022edbd2
force-push-main <10535@holbertonstudents.com>
2025-04-07 02:30:29 +0000

    removed redundant set to nulls

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -14,14 +14,10 @@ void binary_tree_delete(binary_tree_t *tree)
 		return;
 
 	if (tree->left)
-	{
 		binary_tree_delete(tree->left);
-	}
 
 	if (tree->right)
-	{
 		binary_tree_delete(tree->right);
-	}
 
 	free(tree);
 }
