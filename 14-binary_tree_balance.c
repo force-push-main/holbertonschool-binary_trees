71ced2ab0478a7fc221fdffbcf781dfb69b97aad
force-push-main <10535@holbertonstudents.com>
2025-04-08 01:51:34 +0000

    added code for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * find_height - entry point
+ * @tree: root of tree
+ * Description: find height of subtree
+ * Return: height of subtree
+ */
+
 int find_height(const binary_tree_t *tree)
 {
 	int left, right;
@@ -7,8 +14,8 @@ int find_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	left = tree->left ? 1 + find_height(tree->left) : 0;
+	right = tree->right ? 1 + find_height(tree->right) : 0;
 
 	return (left > right ? left : right);
 }
