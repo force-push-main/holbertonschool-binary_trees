eea5645ace619e5631589e503105bd2e945d8677
force-push-main <10535@holbertonstudents.com>
2025-04-08 03:21:52 +0000

    new approach with finding perfect balance in all things

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,28 +13,14 @@ int find_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	left = tree->left ? find_height(tree->left) + 1 : 0;
-	right = tree->right ? find_height(tree->right) + 1 : 0;
 
-	return (left > right ? left : right);
+	left = find_height(tree->left);
+	right = find_height(tree->right);
+	if (left == -1 || right == -1 || abs(left - right) > 0)
+		return (-1);
+
+	return ((left > right ? left : right) + 1);
 }
-
-/**
- * is_full - entry point
- * @tree: root of tree
- * Description: finds if tree is full
- * Return: 1 if full 0 if not or null
- */
-
-int is_full(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	if (is_full(tree->left) != is_full(tree->right))
-		return (0);
-	return (1);
-}
-
 
 /**
  * binary_tree_is_perfect - entry point
@@ -45,16 +31,9 @@ int is_full(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left, right;
-
 	if (!tree)
 		return (0);
-
-	left = tree->left ? find_height(tree->left) + 1 : 0;
-	right = tree->right ? find_height(tree->right) + 1 : 0;
-
-	if ((left - right == 0) && is_full(tree))
-		return (1);
-
-	return (0);
+	if (find_height(tree) == -1)
+		return (0);
+	return (1);
 }
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - entry point
+ * @tree: root of tree
+ * Description: finds if tree is full
+ * Return: finds sibling of node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+}
