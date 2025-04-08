d9168a30d6a54db235da636d5bff194a63ae742c
force-push-main <10535@holbertonstudents.com>
2025-04-08 03:35:03 +0000

    added code for task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,12 +2,18 @@
 
 /**
  * binary_tree_sibling - entry point
- * @tree: root of tree
- * Description: finds if tree is full
+ * @node: node to find sibling of
+ * Description: finds sibling of node
  * Return: finds sibling of node
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (!node->parent || !node)
+		return (NULL);
 
+	if (node == node->parent->left)
+		return (node->parent->right);
+
+	return (node->parent->left);
 }
